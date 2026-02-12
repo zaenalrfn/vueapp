8109e18a3b267c6250fb2422e9d3a7caa8f9f1aa
zaenalrfn <ZAOfficial24@gmail.com>
2026-02-12 21:46:41 +0700

    feat: first commit

---
+++ android/app/src/main/java/com/zaenal/vueapp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.zaenal.vueapp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.zaenal.vueapp',
+  appName: 'vueapp',
+  webDir: 'dist'
+};
+
+export default config;
